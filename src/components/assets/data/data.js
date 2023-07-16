d7b3fc01f9134856f3e8fcbc86dd8deeab70040e
parthib-312 <121247319+parthib-312@users.noreply.github.com>
2023-07-16 09:02:25 +0530

    First Commit

---
+++ src/components/assets/data/data.js
@@ -0,0 +1,384 @@
+import { FcCheckmark } from "react-icons/fc"
+import { AiOutlineClose } from "react-icons/ai"
+
+export const navlist = [
+  {
+    text: "home",
+    path: "/",
+  },
+  {
+    text: "shop",
+    path: "/shop",
+  },
+  {
+    text: "about us",
+    path: "/about",
+  },
+  {
+    text: "blog",
+    path: "/blog",
+  },
+  {
+    text: "contact us",
+    path: "/contact",
+  },
+]
+export const hero = [
+  {
+    id: 1,
+    cover: "https://th.bing.com/th/id/OIP.AbUQMfzAkYJ5xRCI1T8U1gHaHa?pid=ImgDet&rs=1",
+    name: "Dark Themes",
+    items: "5",
+  },
+  {
+    id: 2,
+    cover: "https://th.bing.com/th/id/R.9da7d12d528a7d5e2e4198fc0875cdfb?rik=5WwMGUWyM9tjVQ&riu=http%3a%2f%2fwww.srikanthgiddalur.com%2fimages%2fweb-designer-hyderabad%2fwebsite-design-services-in-hyderabad.png&ehk=bdp10aTKh018dq3pMncWyodCSHChxEyUD7XK5h5ZOP8%3d&risl=&pid=ImgRaw&r=0",
+    name: "Stylish Design",
+    items: "12",
+  },
+  {
+    id: 3,
+    cover: "https://th.bing.com/th/id/R.1b95d42eb8ad7280c75776a1a597f141?rik=bRvLvhYTO%2fybgQ&riu=http%3a%2f%2fwww.pngmart.com%2ffiles%2f4%2f3D-PNG-Image.png&ehk=q98EtTB1ccpATnO5ho%2fpng3hM74qvWtNRp02PM5F4NU%3d&risl=&pid=ImgRaw&r=0",
+    name: "3D Models",
+    items: "12",
+  },
+  {
+    id: 4,
+    cover: "https://static.thenounproject.com/png/700396-200.png",
+    name: "Image Stock",
+    items: "15",
+  },
+  {
+    id: 5,
+    cover: "https://th.bing.com/th/id/OIP.Udg3N1CI9OLnMD-8-fTSSgAAAA?pid=ImgDet&rs=1",
+    name: "Mockup Design",
+    items: "7",
+  },
+]
+export const products = [
+  {
+    id: 1,
+    cover: "https://th.bing.com/th/id/OIP.AbUQMfzAkYJ5xRCI1T8U1gHaHa?pid=ImgDet&rs=1",
+    title: "Transparent 2021 iPhone 12 Cover's",
+    author: "by Blueskytechco",
+    price: 30.0,
+    category: "mockups",
+    qty: 0,
+  },
+  {
+    id: 2,
+    cover: "https://th.bing.com/th/id/OIP.NZix7qtj40ts_hvrpb93KgHaHS?pid=ImgDet&rs=1",
+    title: "Transparent 2021 iPhone 12 Cover's",
+    author: "by Blueskytechco",
+    price: 10.0,
+    category: "mockups",
+    qty: 0,
+  },
+  {
+    id: 3,
+    cover: "https://th.bing.com/th/id/OIP.coUf1XJ1_LFpYihvS3BsDgHaHa?pid=ImgDet&w=800&h=800&rs=1",
+    title: "Dark 2021 iPhone 12 Cover's",
+    author: "by Blueskytechco",
+    price: 20.0,
+    category: "images stocks",
+    qty: 0,
+  },
+  {
+    id: 4,
+    cover: "https://th.bing.com/th/id/OIP.GXmzjuE08cD8-5HfKE5y0AHaHa?pid=ImgDet&w=794&h=794&rs=1",
+    title: "Transparent 2021 iPhone 12 Cover's",
+    author: "by Blueskytechco",
+    price: 40.0,
+    category: "images stocks",
+    qty: 0,
+  },
+  {
+    id: 5,
+    cover: "https://th.bing.com/th/id/OIP._4niLLSsY6mKDPHC3UlWUgAAAA?pid=ImgDet&w=320&h=320&rs=1",
+    title: "Dark 2021 iPhone 12 Cover's",
+    author: "by Blueskytechco",
+    price: "50.00",
+    category: "dark theme",
+    qty: 0,
+  },
+  {
+    id: 6,
+    cover: "https://th.bing.com/th/id/OIP.UxEoGAa-fTLrOgNUooLC0AHaHa?pid=ImgDet&w=1000&h=1000&rs=1",
+    title: "Dark 2021 iPhone 12 Cover's",
+    author: "by Blueskytechco",
+    price: "30.00",
+    category: "dark theme",
+    qty: 0,
+  },
+  {
+    id: 7,
+    cover: "https://cf.shopee.ph/file/2b02a7ddb47f4546ec450f4c32ea3c54",
+    title: "Dark 2021 iPhone 12 Cover's",
+    author: "by Blueskytechco",
+    price: "30.00",
+    category: "icons",
+    qty: 0,
+  },
+  {
+    id: 8,
+    cover: "https://i.pinimg.com/736x/4d/c1/bf/4dc1bf0b8c495c15321f8aa76fee000a.jpg",
+    title: "Dark 2021 iPhone 12 Cover's",
+    author: "by Blueskytechco",
+    price: "30.00",
+    category: "icons",
+    qty: 0,
+  },
+]
+export const banner = [
+  {
+    id: 1,
+    title1: "Simple IPhone ",
+    title2: "Mockups Design PSD, Ai, EPS",
+    desc: "Sale 30% Off For Members",
+    cover: "https://th.bing.com/th/id/OIP.Nn7DHWcp2pSHYlXTddtxXQHaFh?pid=ImgDet&w=768&h=573&rs=1",
+  },
+  {
+    id: 2,
+    title1: "Mockup PSD",
+    title2: "50+ Ui Screen Mockups",
+    desc: "Sale 30% Off For Members",
+    cover: "https://th.bing.com/th/id/OIP.WLSTeiSs_wgIoDTU_tiO2gHaFm?pid=ImgDet&w=820&h=620&rs=1",
+  },
+]
+export const topProducts = [
+  {
+    id: 20,
+    cover: "https://i.pinimg.com/originals/c6/9c/8c/c69c8c969b89749a1664ff6016537202.jpg",
+    title: "Floating 2021 iPhone 12 Mockups (PSD)",
+    author: "by Blueskytechco in Mockups",
+    price: "30.00",
+    category: "icons",
+    qty: 0,
+  },
+  {
+    id: 21,
+    cover: "https://th.bing.com/th/id/R.97c9f94ae5782b423e4060bbae2f8dd7?rik=tXFUuCO0JreikQ&riu=http%3a%2f%2fwww.themobileindian.com%2fimages%2freviews%2f2013%2f06%2f823%2fPanasonic-P51-16.jpg&ehk=NlJ2owscGDAclXr2S6QrCuUjTmSp7ALROiqaFri0xkk%3d&risl=&pid=ImgRaw&r=0",
+    title: "Floating 2021 iPhone 12 Mockups (PSD)",
+    author: "by Blueskytechco in Mockups",
+    price: "30.00",
+    category: "icons",
+    qty: 0,
+  },
+  {
+    id: 22,
+    cover: "https://i.pinimg.com/originals/55/bc/1e/55bc1e67030df77b44e6637a2b7247ea.jpg",
+    title: "Floating 2021 iPhone 12 Mockups (PSD)",
+    author: "by Blueskytechco in Mockups",
+    price: "30.00",
+    category: "dark theme",
+    qty: 0,
+  },
+
+  {
+    id: 23,
+    cover: "https://cdn.dribbble.com/users/267808/screenshots/1704298/-dribble.jpg",
+    title: "Floating 2021 iPhone 12 Mockups (PSD)",
+    author: "by Blueskytechco in Mockups",
+    price: "30.00",
+    category: "dark theme",
+    qty: 0,
+  },
+  {
+    id: 24,
+    cover: "https://th.bing.com/th/id/OIP.xfX8Tm1QmKSjWxSnq8mBfAHaFj?pid=ImgDet&w=1400&h=1050&rs=1",
+    title: "Floating 2021 iPhone 12 Mockups (PSD)",
+    author: "by Blueskytechco in Mockups",
+    price: "30.00",
+    category: "images stocks",
+    qty: 0,
+  },
+  {
+    id: 25,
+    cover: "https://i.pinimg.com/originals/3a/9d/87/3a9d87e345745a89d8197d35c13d6ac1.jpg",
+    title: "Floating 2021 iPhone 12 Mockups (PSD)",
+    author: "by Blueskytechco in Mockups",
+    price: "30.00",
+    category: "images stocks",
+    qty: 0,
+  },
+  {
+    id: 26,
+    cover: "https://cdn.dribbble.com/users/39493/screenshots/1199130/sports-app.jpg?compress=1&resize=600x450",
+    title: "Floating 2021 iPhone 12 Mockups (PSD)",
+    author: "by Blueskytechco in Mockups",
+    price: "30.00",
+    category: "mockups",
+    qty: 0,
+  },
+  {
+    id: 27,
+    cover: "https://th.bing.com/th/id/OIP.FusioHc_EuD1yJKB9Q_b3gHaFj?pid=ImgDet&w=800&h=600&rs=1",
+    title: "Floating 2021 iPhone 12 Mockups (PSD)",
+    author: "by Blueskytechco in Mockups",
+    price: "30.00",
+    category: "mockups",
+    qty: 0,
+  },
+]
+export const price = [
+  {
+    id: 1,
+    name: "Free Trial",
+    price: "0",
+    desc: "Beautifully simple project planning, 14 days of free trial.",
+    list: [
+      {
+        para: "Demo Content Install",
+        icon: <FcCheckmark />,
+      },
+      {
+        para: "Theme Updates",
+        icon: <FcCheckmark />,
+      },
+      {
+        para: "Support And Updates",
+        icon: <AiOutlineClose />,
+      },
+      {
+        para: "Access All Themes",
+        icon: <AiOutlineClose />,
+      },
+      {
+        para: "All Themes For Life",
+        icon: <AiOutlineClose />,
+      },
+      {
+        para: "30 Days Money Back",
+        icon: <AiOutlineClose />,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Premium",
+    price: "26",
+    desc: "Declutter your mind and save time with Premium.",
+    list: [
+      {
+        para: "Demo Content Install",
+        icon: <FcCheckmark />,
+      },
+      {
+        para: "Theme Updates",
+        icon: <FcCheckmark />,
+      },
+      {
+        para: "Support And Updates",
+        icon: <FcCheckmark />,
+      },
+      {
+        para: "Access All Themes",
+        icon: <FcCheckmark />,
+      },
+      {
+        para: "All Themes For Life",
+        icon: <AiOutlineClose />,
+      },
+      {
+        para: "30 Days Money Back",
+        icon: <AiOutlineClose />,
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Unlimited Access ",
+    price: "49",
+    desc: "Declutter your mind and save time with Premium.",
+    list: [
+      {
+        para: "Demo Content Install",
+        icon: <FcCheckmark />,
+      },
+      {
+        para: "Theme Updates",
+        icon: <FcCheckmark />,
+      },
+      {
+        para: "Support And Updates",
+        icon: <FcCheckmark />,
+      },
+      {
+        para: "Access All Themes",
+        icon: <FcCheckmark />,
+      },
+      {
+        para: "All Themes For Life",
+        icon: <FcCheckmark />,
+      },
+      {
+        para: "30 Days Money Back",
+        icon: <FcCheckmark />,
+      },
+    ],
+  },
+]
+export const customer = [
+  {
+    id: 1,
+    desc: "Congue condimentum et non eu arcu sociis aenean vivamus quisque. Porta purus urna. Massa id blandit enim cursus ante, nec consectetuer imperdiet ipsum",
+    name: "ALGISTINO",
+    post: "Marketing Company Director",
+  },
+  {
+    id: 2,
+    desc: "Congue condimentum et non eu arcu sociis aenean vivamus quisque. Porta purus urna. Massa id blandit enim cursus ante, nec consectetuer imperdiet ipsum",
+    name: "ALGISTINO",
+    post: "Marketing Company Director",
+  },
+  {
+    id: 3,
+    desc: "Congue condimentum et non eu arcu sociis aenean vivamus quisque. Porta purus urna. Massa id blandit enim cursus ante, nec consectetuer imperdiet ipsum",
+    name: "ALGISTINO",
+    post: "Marketing Company Director",
+  },
+]
+export const blog = [
+  {
+    id: 1,
+    date: "October 27, 2021",
+    title: "Transition Your Favorite Looks into  Fall France 2022",
+    category: "image",
+    cover: "https://th.bing.com/th/id/OIP._Au8r2lW_dA7V2uHDhqZhQHaEL?pid=ImgDet&w=600&h=338&rs=1",
+  },
+  {
+    id: 2,
+    date: "October 27, 2021",
+    title: "Meeting Breathtaking Beauty for  Everyday Life",
+    category: "shopping",
+    cover: "https://th.bing.com/th/id/OIP._Au8r2lW_dA7V2uHDhqZhQHaEL?pid=ImgDet&w=600&h=338&rs=1",
+  },
+  {
+    id: 3,
+    date: "October 27, 2021",
+    title: "Perfect Quality Reasonable Price for Your",
+    category: "travle",
+    cover: "https://th.bing.com/th/id/OIP._Au8r2lW_dA7V2uHDhqZhQHaEL?pid=ImgDet&w=600&h=338&rs=1",
+  },
+  {
+    id: 4,
+    date: "October 27, 2021",
+    title: "Transition Your Favorite Looks into  Fall France 2022",
+    category: "music",
+    cover: "https://th.bing.com/th/id/OIP._Au8r2lW_dA7V2uHDhqZhQHaEL?pid=ImgDet&w=600&h=338&rs=1",
+  },
+  {
+    id: 5,
+    date: "October 27, 2021",
+    title: "Meeting Breathtaking Beauty for  Everyday Life",
+    category: "music",
+    cover: "https://th.bing.com/th/id/OIP._Au8r2lW_dA7V2uHDhqZhQHaEL?pid=ImgDet&w=600&h=338&rs=1",
+  },
+  {
+    id: 6,
+    date: "October 27, 2021",
+    title: "Perfect Quality Reasonable Price for Your",
+    category: "lifestyle",
+    cover: "https://th.bing.com/th/id/OIP._Au8r2lW_dA7V2uHDhqZhQHaEL?pid=ImgDet&w=600&h=338&rs=1",
+  },
+]
